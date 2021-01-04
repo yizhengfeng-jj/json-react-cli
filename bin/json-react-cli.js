0346273d1a44768745a954776918cd62f4592423
lvjie04 <lvjie04@baidu.com>
2021-01-04 15:55:01 +0800

    lvjie: init package

---
+++ bin/json-react-cli.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node 
+const path = require('path');
+const commander = require('commander');
+const package = require('../package.json');
+const {sync: glob} = require('glob');
+
+const execCommand = (commander, path) => {
+    const {command, run, description, args} = require(path);
+    const comConfig = commander.command(command); // 存入命令配置
+
+    args.forEach(arg => {
+        comConfig.option(...arg);
+    })
+    comConfig.description(description);
+    comConfig.action(run);
+}
+
+function run() {
+    const paths = glob(path.join(__dirname, '../lib/cli/*.js'));
+
+    paths.forEach(path => {
+        execCommand(commander, path);
+    });
+
+    commander.version(package.version);
+    commander.parse(process.argv);
+
+    // console.log(commander.force, 'commander...commander...');
+}
+
+run();
