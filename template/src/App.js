89c568f1adae4fa53bff5a1ef0ad8318e0f61fa0
lvjie04 <lvjie04@baidu.com>
2021-01-08 14:03:06 +0800

    feature:更改template模版

--- template/src/App.js
+++ template/src/App.js
@@ -1,5 +1,15 @@
-console.log('a');
+import React from 'react';
+import ReactDom from 'react-dom';
 
-if(module.hot) {
-    module.hot.accept();
+import './App.less';
+
+class App extends React.Component {
+    render() {
+        return <div className="container">
+            <div><img src="https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/154c9711cfee46b68400f4f1eedb9983~tplv-k3u1fbpfcp-watermark.image" /></div>
+            <div className="title">这是风恋猫的脚手架，持续更新中敬请期待...</div>
+        </div>
+    }
 }
+
+ReactDom.render(<App />, document.getElementById('root'))
